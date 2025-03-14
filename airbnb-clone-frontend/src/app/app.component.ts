f89113765a189181264307837bb8f12d7fc67856
Lucas Lima <lucaserolima@gmail.com>
2025-03-14 16:04:35 -0400

    Install FontAwesome packages for Angular integration

--- airbnb-clone-frontend/src/app/app.component.ts
+++ airbnb-clone-frontend/src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ButtonModule } from 'primeng/button';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'airbnb-clone-frontend';
+export class AppComponent implements OnInit {
+
+  faIconLibrary: FaIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
