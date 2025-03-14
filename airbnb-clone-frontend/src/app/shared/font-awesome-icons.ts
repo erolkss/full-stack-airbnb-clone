f077fb9f0edb432089f190751f7d61f0de19844c
Lucas Lima <lucaserolima@gmail.com>
2025-03-14 16:34:39 -0400

    feat: add conditional user avatar with fallback icon

--- airbnb-clone-frontend/src/app/shared/font-awesome-icons.ts
+++ airbnb-clone-frontend/src/app/shared/font-awesome-icons.ts
@@ -1,6 +1,8 @@
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { faAirbnb } from "@fortawesome/free-brands-svg-icons";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
 
 export const fontAwesomeIcons: IconDefinition[] = [
-    faAirbnb
+    faAirbnb,
+    faUser
 ]
