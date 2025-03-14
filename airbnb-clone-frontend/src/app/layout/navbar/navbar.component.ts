6603186bb9fad680de18fc8519d6364417bcf595
Lucas Lima <lucaserolima@gmail.com>
2025-03-14 18:26:51 -0400

    feat(navbar): implement Airbnb-style navigation bar with category component

---
+++ airbnb-clone-frontend/src/app/layout/navbar/navbar.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ButtonModule } from 'primeng/button';
+import { CategoryComponent } from './category/category.component';
+import { AvatarComponent } from './avatar/avatar.component';
+import { ToolbarModule } from 'primeng/toolbar';
+import { MenuModule } from 'primeng/menu';
+
+import { DialogService } from 'primeng/dynamicdialog';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    ButtonModule,
+    FontAwesomeModule,
+    ToolbarModule,
+    MenuModule,
+    CategoryComponent,
+    AvatarComponent,
+  ],
+  providers: [DialogService],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent implements OnInit {
+  location = 'Anywhere';
+  guests = 'Add guests';
+  dates = 'Any week';
+
+  // login () => this.authService.login();
+  // logout () => this.authService.logout();
+
+  currentMenuItems: MenuItem[] | undefined = [];
+
+  ngOnInit(): void {
+    this.fetchMenu();
+  }
+
+  fetchMenu() {
+    return [
+      {
+        label: 'Sign up',
+        styleClass: 'font-bold',
+      },
+      {
+        label: 'Log in',
+      },
+    ];
+  }
+}
