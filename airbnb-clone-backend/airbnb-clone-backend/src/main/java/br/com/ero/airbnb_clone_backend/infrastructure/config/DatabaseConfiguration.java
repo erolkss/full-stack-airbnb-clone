718953eff9b8601990a421c48850ba8e3a897564
Lucas Lima <lucaserolima@gmail.com>
2025-03-15 12:08:50 -0400

    chore(database): add booking repository to JPA configuration

--- airbnb-clone-backend/airbnb-clone-backend/src/main/java/br/com/ero/airbnb_clone_backend/infrastructure/config/DatabaseConfiguration.java
+++ airbnb-clone-backend/airbnb-clone-backend/src/main/java/br/com/ero/airbnb_clone_backend/infrastructure/config/DatabaseConfiguration.java
@@ -6,7 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableJpaRepositories({"br.com.ero.airbnb_clone_backend.user.repository", "br.com.ero.airbnb_clone_backend.listing.repository"})
+@EnableJpaRepositories({
+        "br.com.ero.airbnb_clone_backend.user.repository",
+        "br.com.ero.airbnb_clone_backend.listing.repository",
+        "br.com.ero.airbnb_clone_backend.booking.repository"})
 @EnableTransactionManagement
 @EnableJpaAuditing
 public class DatabaseConfiguration {
