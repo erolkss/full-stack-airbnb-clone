d116a8b76fc589893c809115a82b28c0d00a66eb
Lucas Lima <lucaserolima@gmail.com>
2025-03-14 20:54:54 -0400

    Initial commit: Set up backend project for airbnb-clone-backend

---
+++ airbnb-clone-backend/airbnb-clone-backend/src/main/java/br/com/ero/airbnb_clone_backend/AirbnbCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.ero.airbnb_clone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnbCloneBackendApplication.class, args);
+	}
+
+}
---
+++ airbnb-clone-backend/airbnb-clone-backend/src/test/java/br/com/ero/airbnb_clone_backend/AirbnbCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ero.airbnb_clone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
