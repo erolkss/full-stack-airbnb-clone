8b5b50748523089c05592b7fcf201b95d25d5334
Lucas Lima <lucaserolima@gmail.com>
2025-03-14 20:02:01 -0400

    feat(layout): add navbar and footer to main layout

--- airbnb-clone-frontend/src/app/app.component.ts
+++ airbnb-clone-frontend/src/app/app.component.ts
@@ -3,18 +3,21 @@ import { RouterOutlet } from '@angular/router';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ButtonModule } from 'primeng/button';
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { NavbarComponent } from "./layout/navbar/navbar.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, FontAwesomeModule],
+  imports: [RouterOutlet, ButtonModule, FontAwesomeModule, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
 
   faIconLibrary: FaIconLibrary = inject(FaIconLibrary);
+  isListingView: boolean = true;
 
   ngOnInit(): void {
     this.initFontAwesome();
