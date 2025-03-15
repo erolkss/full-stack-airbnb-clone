3316f933f4c58bab98049cc7e84f45598118e1dc
Lucas Lima <lucaserolima@gmail.com>
2025-03-15 11:07:27 -0400

    chore(database): enable JPA repositories and auditing

---
+++ airbnb-clone-backend/airbnb-clone-backend/src/main/java/br/com/ero/airbnb_clone_backend/infrastructure/config/DatabaseConfiguration.java
@@ -0,0 +1,13 @@
+package br.com.ero.airbnb_clone_backend.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories({"br.com.ero.airbnb_clone_backend.user.repository"})
+@EnableTransactionManagement
+@EnableJpaAuditing
+public class DatabaseConfiguration {
+}
