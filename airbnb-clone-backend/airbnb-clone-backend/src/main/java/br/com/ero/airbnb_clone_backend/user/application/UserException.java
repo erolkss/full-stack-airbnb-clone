8bb57596be40f17364a792e8396c59e86f543eda
Lucas Lima <lucaserolima@gmail.com>
2025-03-17 15:56:49 -0400

    feat(user): add custom UserException for user-related errors

---
+++ airbnb-clone-backend/airbnb-clone-backend/src/main/java/br/com/ero/airbnb_clone_backend/user/application/UserException.java
@@ -0,0 +1,8 @@
+package br.com.ero.airbnb_clone_backend.user.application;
+
+public class UserException extends RuntimeException {
+
+    public UserException(String message) {
+        super(message);
+    }
+}
