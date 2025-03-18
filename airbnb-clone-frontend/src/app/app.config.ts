ac63cb778836c443c5fd039662a23a43bd15ff24
Lucas Lima <lucaserolima@gmail.com>
2025-03-18 19:57:06 -0400

    feat(auth): implement user authentication state management using Signals
    - Added fetchUser$ signal to store authenticated user state
    - Implemented fetch method to retrieve user data via HTTP
    - Handled unauthorized error cases by setting NOT_CONNECTED status
    - Added isAuthenticated method to check user's authentication status

--- airbnb-clone-frontend/src/app/app.config.ts
+++ airbnb-clone-frontend/src/app/app.config.ts
@@ -4,9 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { provideAnimations } from '@angular/platform-browser/animations'
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
+    provideHttpClient(),
     provideRouter(routes)]
 };
