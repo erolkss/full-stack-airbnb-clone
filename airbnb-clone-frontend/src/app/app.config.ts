5b2d2dfcfed9006593032b89358f6305563fc097
Lucas Lima <lucaserolima@gmail.com>
2025-03-19 15:06:09 -0400

    feat(http): configure HttpClient with authExpired interceptor and XSRF protection

--- airbnb-clone-frontend/src/app/app.config.ts
+++ airbnb-clone-frontend/src/app/app.config.ts
@@ -4,11 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { provideAnimations } from '@angular/platform-browser/animations'
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withXsrfConfiguration } from '@angular/common/http';
+import { authExpired } from './core/core/auth-expired.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([authExpired]),
+      withXsrfConfiguration({ cookieName: "XSRF-TOKEN", headerName: "X-XSRF-TOKEN" })
+    ),
     provideRouter(routes)]
 };
